6beae1b2156542eac2a66e10ba5a0bd8fd56eed2
Bae Hyeonseo <haulqogustj@naver.com>
2024-04-06 01:07:23 +0900

    cicd

---
+++ src/main/java/com/back/kukertonc/domain/health/healthController.java
@@ -0,0 +1,16 @@
+package com.back.kukertonc.domain.health;
+
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class healthController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
