e2a9827c493929cd5998d1c57ee5bbc20238d641
Bae Hyeonseo <haulqogustj@naver.com>
2024-04-05 20:23:43 +0900

    cicd

---
+++ src/main/java/com/back/kukertonc/KukertoncApplication.java
@@ -0,0 +1,13 @@
+package com.back.kukertonc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukertoncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukertoncApplication.class, args);
+	}
+
+}
